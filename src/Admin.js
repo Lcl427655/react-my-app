a091a8e30b9e5b95db875ea969c39b4de4d85b1a
lcl <26427655@qq.com>
2019-09-01 14:46:50 +0800

    test

--- src/Admin.js
+++ src/Admin.js
@@ -3,21 +3,25 @@ import { Row, Col } from 'antd';
 import NavLeft from './components/NavLeft';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import './style/common.less'
 export default class Admin extends React.Component {
     render() {
         return <div>
-            <Row>
-                <Col span={3}>
-                    <NavLeft></NavLeft>
+            <Row className="container">
+                {/* 左侧菜单 */}
+                <Col span={3} className="nav-left">
+                    <NavLeft/>
                 </Col>
-                <Col span={21}>
-                    <Header>
-
-                    </Header>
-                    <Row>
+                {/* 右侧内容 */}
+                <Col span={21} className="main">
+                    {/* 头部 */}
+                    <Header/>
+                    {/* 内容区域 */}
+                    <Row className="content">
                         content
                     </Row>
-                    <Footer></Footer>
+                    {/* 底部 */}
+                    <Footer/>
                 </Col>
             </Row>
         </div>
--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,34 +1,44 @@
 import React from 'react';
-import { Menu, Icon } from 'antd';
-//import MenuConfig from '../../config/menuConfig';
+import { Menu} from 'antd';
+import MenuConfig from '../../config/menuConfig';
 import './index.less';
 const { SubMenu } = Menu;
 export default class NavLeft extends React.Component{
-    render(){
-        return <div className="nav-left">
-            <div className="logo">
-                <img src="/assets/logo-ant.svg" alt=""/>
-                <h1>Imooc MS</h1>
-            </div>
-            <div>
-            <Menu
-                theme='dark'
-            >
-                <SubMenu key="sub1"
-                  title={
-                    <span>
-                      <Icon type="mail" />
-                      <span>Navigation One</span>
-                    </span>
-                  }>
-                    <Menu.Item key="1">Option 1</Menu.Item>
-                    <Menu.Item key="2">Option 2</Menu.Item>
-                    <Menu.Item key="3">Option 3</Menu.Item>
-                    <Menu.Item key="4">Option 4</Menu.Item>
-                </SubMenu>
-              </Menu>
-            </div>
-        </div>
-    }
+  componentWillMount(){
+    const menuTreeNode = this.renderMenu(MenuConfig);
+    this.setState({
+      menuTreeNode
+    }) 
+  }
+  // 菜单渲染
+  renderMenu =(data)=>{
+    return data.map((item)=>{
+      if(item.children){
+        return (
+          <SubMenu title={item.title} key={item.key}>
+              { this.renderMenu(item.children)}
+          </SubMenu>
+        )
+      }
+      return <Menu.Item title={item.title} key={item.key}>
+        {item.title}
+      </Menu.Item>
+    })
+  }
+  render(){
+      return <div className="nav-left">
+          <div className="logo">
+              <img src="/assets/logo-ant.svg" alt=""/>
+              <h1>Imooc MS</h1>
+          </div>
+          <div>
+          <Menu
+              theme='dark'
+          >
+              {this.state.menuTreeNode}
+            </Menu>
+          </div>
+      </div>
+  }
 }
 
